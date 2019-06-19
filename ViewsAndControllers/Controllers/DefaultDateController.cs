f4a4d2195a2e00b67a51b81dd167318688c33d7e
Aaron Melcher <amelch130@gmail.com>
2019-06-19 11:43:10 -0500

    Added default date

---
+++ ViewsAndControllers/Controllers/DefaultDateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class DefaultDateController : Controller
+    {
+        // GET: DefaultDate
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: "");
+        }
+
+        public ActionResult DisplayTodaysDate()
+        {
+            return View(viewName: "Index", model: DateTime.Today.ToString("MM/dd/yy"));
+        }
+    }
+}
